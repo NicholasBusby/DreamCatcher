cd5fcb4fc2ebe4d5cb8f496ffd8b8ffa8c0be32f
Nicholas Busby <Nicholas.Busby@Live.com>
2018-01-15 20:40:41 -0800

    background is scrolling now

---
+++ Assets/GameControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public float BackgroundSpeed = -1.5f;
+
+    public static GameControl Instance;
+
+    private void Awake()
+    {
+        Instance = Instance ?? this;
+
+        if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Renderer render;
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        render = GetComponent<Renderer>();
+        rb2d.velocity = new Vector2(0, GameControl.Instance.BackgroundSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var viewPortPosition = Camera.main.WorldToViewportPoint(rb2d.position);
+        if(viewPortPosition.y < -.75f)
+        {
+            rb2d.position = new Vector2(rb2d.position.x, (render.bounds.size.y * 3) + rb2d.position.y);
+        }
+	}
+}
